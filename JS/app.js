ce880da7c7247898492d18ea925cbcf085db66ee
Azur-Azabache <tharuka_20@hotmail.com>
2017-11-20 01:03:49 -0500

    Añadiendo funcion contadora

--- JS/app.js
+++ JS/app.js
@@ -4,6 +4,8 @@ var button = document. getElementById('tweet');
 var message = document.getElementById('message');
 var register = moment().format('LT');
 var slash = document.getElementById('slash');
+var number = document.getElementById('number');
+
 //Funcion tweetear
 button.addEventListener('click', function(event) {
   var text = textArea.value;
@@ -12,10 +14,21 @@ button.addEventListener('click', function(event) {
   event.preventDefault();
   textArea.value = '¡Necesitas llenar este espacio!';
   }else{
-  box.textContent = text;
+  box.innerHTML = text + '\n' + register;
   message.appendChild(box);
-
 }
+})
+//Funcion contadora de carácteres
+textArea.addEventListener('keyup', function(){
+  var counter = 140 - (document.getElementById('textArea').value.length);
+  document.getElementById('number').textContent = counter;
+  if (counter <= 120){
+    number.style.color = 'red';
+  } else if (counter <= 130){
+    number.style.color = 'blue';
+  } else if(counter >130){
+    number.style.color = '#088A85';
+  }
 
 })
 
